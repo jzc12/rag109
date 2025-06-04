8266f37901e361baeaad8a1458b13aea3b0aa489
221900323 <221900323@smail.nju.edu.cn>
2025-06-04 22:02:46 +0800

    add fisrt

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
-export default defineConfig({//
+export default defineConfig({
+  base: '/rag109/',  // 确保这里的名称与你的 GitHub 仓库名一致
   plugins: [react()],
   server: {                                     // 允许内网穿透的映射网址公网访问
     host: '0.0.0.0',
