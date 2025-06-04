de40d3e0c7b73c67ceb6d77e84f6470ecbf06350
221900323 <221900323@smail.nju.edu.cn>
2025-06-04 21:50:17 +0800

    add fisrt

--- src/utils/url.js
+++ src/utils/url.js
@@ -3,7 +3,3 @@
 export const get_backend_url = () => {
     return import.meta.env.VITE_API_BASE_URL;
 };
-
-export const get_front_url = () => {
-    return import.meta.env.VITE_FRONT_URL;
-};
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rag109/',
   plugins: [react()],
   server: {                                     // 允许内网穿透的映射网址公网访问
     host: '0.0.0.0',
