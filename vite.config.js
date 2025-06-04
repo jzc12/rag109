8472604cc17f5de0680fcb2f81d0fec543c727c1
221900323 <221900323@smail.nju.edu.cn>
2025-06-04 23:05:24 +0800

    modify back url

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   plugins: [react()],
   server: {                                     // 允许内网穿透的映射网址公网访问
     host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: ['108sn64tu8943.vicp.fun']
+    port: 9090,
+    allowedHosts: ['https://108sn64tu8943.vicp.fun']
   },
   resolve: {
     alias: {
