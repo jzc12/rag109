5938615d9f0c16231f0b2dedbf0934fbabc9520c
221900323 <221900323@smail.nju.edu.cn>
2025-06-06 21:39:29 +0800

    add video link

--- src/pages/BackendOff.jsx
+++ src/pages/BackendOff.jsx
@@ -6,7 +6,7 @@ const BackendOff = () => {
         <div className="backend-off-container">
             <h1>Backend Offline</h1>
             <p>We are currently unable to connect to the backend server. Please try again later.</p>
-            <p>If the problem persists, please contact support.</p>
+            <p>You can see <a href="https://www.bilibili.com/video/BV139TNzTEN6/">our video</a> to know how our system works</p>
         </div>
     );
 };
